f7f1d60f4b62be8212dedf70b75530806d1a56d6
lucaslmota <lucaslmota0431@gmail.com>
2022-05-12 18:41:31 -0300

    CRUD simples com dapper

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -10,10 +10,13 @@ namespace Blog
         private const string ConnectionString = "Server=localhost,1433;Database=Blog;User Id=sa;Password=042393ll;Trusted_Connection=False; TrustServerCertificate=True;";
         static void Main(string[] args)
         {
-            ReadUser();
+            //ReadUsers();
+            //ReadUser();
+            //CreatUser();
+            UpdateUser();
         }
 
-        public static void ReadUser()
+        public static void ReadUsers()
         {
             using (var connection = new SqlConnection(ConnectionString))
             {
@@ -23,6 +26,65 @@ namespace Blog
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+
+            }
+        }
+
+        public static void CreatUser()
+        {
+            var createUser = new User()
+            {
+                Name = "Débora dos bodes",
+                Email = "debes@gmail.com",
+                PasswordHash = "Hash",
+                Bio = "cuidadora de bode",
+                Image = "https://",
+                Slug = "debes-bode"
+            };
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Insert<User>(createUser);
+                Console.WriteLine("Cadastro realizado com sucesso!");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var updateUser = new User()
+            {
+                Id = 1,
+                Name = "Lucas Mota",
+                Email = "lulu@gmail.com",
+                PasswordHash = "Hash",
+                Bio = "Dev c#",
+                Image = "https://",
+                Slug = "dev-lucas"
+            };
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Update<User>(updateUser);
+                Console.WriteLine("Usuário atualizado com sucesso!");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var deleteUser = connection.Get<User>(1);
+                connection.Delete<User>(deleteUser);
+                Console.WriteLine("Usuário deletado!");
             }
         }
     }
