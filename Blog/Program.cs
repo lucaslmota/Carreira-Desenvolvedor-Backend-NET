6e232675ea572588aa07634dd858a04f0a15c3df
lucaslmota <lucaslmota0431@gmail.com>
2022-05-15 18:31:53 -0300

    Ajustes na Tag para replicar nas demais e completar alguns desafios

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Blog.Models;
 using Blog.Repositories;
-using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 namespace Blog
@@ -13,67 +12,13 @@ namespace Blog
         {
             var connection = new SqlConnection(ConnectionString);
             connection.Open();
-            // ReadUsers(connection);
-            // ReadRoles(connection);
 
-            ReadUserWithRoles(connection);
-
+            Console.ReadKey();
             connection.Close();
+
         }
 
-        public static void ReadUsers(SqlConnection connection)
-        {
 
-            var repository = new Repository<User>(connection);
-            var users = repository.GetAll();
-
-            foreach (var user in users)
-            {
-                Console.WriteLine(user.Name);
-
-                foreach (var role in user.Roles)
-                {
-                    Console.WriteLine($"- {role.Name}");
-                }
-            }
-        }
-
-        public static void ReadUserWithRoles(SqlConnection connection)
-        {
-            var repository = new UserRepository(connection);
-            var users = repository.GetWithRoles();
-
-            foreach (var user in users)
-            {
-                Console.WriteLine(user.Name);
-                foreach (var role in user.Roles)
-                {
-                    Console.WriteLine($" - {role.Name}");
-                }
-            }
-        }
-
-        public static void ReadRoles(SqlConnection connection)
-        {
-            var repository = new Repository<Role>(connection);
-            var roles = repository.GetAll();
-
-            foreach (var role in roles)
-            {
-                Console.WriteLine(role.Name);
-            }
-        }
-
-        public static void ReadTag(SqlConnection connection)
-        {
-            var repository = new Repository<Tag>(connection);
-            var tags = repository.GetAll();
-
-            foreach (var tag in tags)
-            {
-                Console.WriteLine(tag.Name);
-            }
-        }
 
     }
 }
